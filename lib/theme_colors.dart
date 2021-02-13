431f9c74d3e1f55d3ae77b7fc1b66c2b08232e77
Adrian K <adrian@Macmac.local>
2021-02-13 13:11:47 +0100

    fix(kudos-wall): remove unused color

--- lib/theme_colors.dart
+++ lib/theme_colors.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class ThemeColors {
   static const Color lightGrey = Color.fromRGBO(223, 229, 235, 1);
   static const Color darkGrey = Color.fromRGBO(64, 64, 64, 1);
-  static const Color neutralGreen = Color.fromRGBO(104, 182, 132, 1);
 
   static const Color mainFontColor = darkGrey;
   static const Color tileBorderColor = lightGrey;
